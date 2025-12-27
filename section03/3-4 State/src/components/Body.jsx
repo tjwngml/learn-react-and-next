e39ffb82a4782ad946bcd6414fa8f5d6c78c01bc
tjwngml <tjwngml0306@gmail.com>
2025-12-27 22:25:15 +0900

    3-6 State로 사용자 입력 관리하기 (2)

--- section03/3-4 State/src/components/Body.jsx
+++ section03/3-4 State/src/components/Body.jsx
@@ -1,43 +1,37 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
 export default function Body(){
   
-  const [name, setName] = useState("");
-  const [gender, setGender] = useState("");
-  const [bio, setBio] = useState("");
+  const [state, setState] = useState({
+    name:"",
+    gender:"",
+    bio:""
+  });
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
-    // console.log(name); 리렌더링이 되기 전이기 때문에 콘솔 찍으면 바로 반영되지 않음
-    console.log(e.target.value); // e.target.value 로 콘솔에서 확인 가능
-  };
-  
-
-  const onChangeGender = (e) => {
-    setGender(e.target.value);
-    console.log(e.target.value);
-  }
-
-  const onChangeBio= (e) => {
-    setBio(e.target.value);
-    console.log(e.target.value);
-  }
+ const onChange =(e)=> {
+  setState({
+    ...state,
+    [e.target.name]: e.target.value,
+    // 객체 생성 시 대괄호와 함께 프로퍼티의 name을 쓰는 것 -> 계산된 프로퍼티 
+    // computed property . e.target.name에 저장된 값을 프로퍼티의 key로 사용함. 
+    });
+ }
 
   return(
     <div className="body">
       <div>
-      <input value={name} onChange={onChangeName} />
+      <input name={"name"} value={state.name} onChange={onChange} />
       </div>
     
       <div>
-        <select value={gender} onChange={onChangeGender}>
+        <select name={"gender"} value={state.gender} onChange={onChange}>
           <option value=""> 밝히지 않음</option>
           <option value="female"> 여성</option>
           <option value="male"> 남성</option>
         </select>
       </div>
       <div>
-        <textarea value={bio} onChange={onChangeBio}/>
+        <textarea name={"bio"} value={state.bio} onChange={onChange}/>
       </div>
     </div>
   );
