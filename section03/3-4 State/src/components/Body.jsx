1a6084783123c9744f8fcd26fadc8eba0553da6f
tjwngml <tjwngml0306@gmail.com>
2025-12-27 22:14:11 +0900

    3-6 State로 사용자 입력 관리하기 (1)

--- section03/3-4 State/src/components/Body.jsx
+++ section03/3-4 State/src/components/Body.jsx
@@ -1,34 +1,46 @@
-import { use, useState } from "react";
-
-function Ligthbulb({light}) {
-  return ( 
-   <>
-    {light === "ON" ? (
-      <div style={{backgroundColor: "orange"}}> ON</div>) : (
-      <div style={{backgroundColor:"gray"}}> OFF </div>
-    )}
-  </>
-  );
-}
-
-
-function StaticLightbulb(){
-  return <div style={{backgroundColor:"gray"}}>OFF</div>;
-}
-
+import { useState } from "react";
 
 export default function Body(){
   
-  const [light, setLight] = useState("OFF");
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [bio, setBio] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+    // console.log(name); 리렌더링이 되기 전이기 때문에 콘솔 찍으면 바로 반영되지 않음
+    console.log(e.target.value); // e.target.value 로 콘솔에서 확인 가능
+  };
+  
+
+  const onChangeGender = (e) => {
+    setGender(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const onChangeBio= (e) => {
+    setBio(e.target.value);
+    console.log(e.target.value);
+  }
 
   return(
     <div className="body">
-      <Ligthbulb light={light}/>
-      <StaticLightbulb/>
-      <button onClick={()=>{setLight("ON");}}>켜기</button>
-      <button onClick={()=>{setLight("OFF");}}>끄기</button>
-      
+      <div>
+      <input value={name} onChange={onChangeName} />
+      </div>
+    
+      <div>
+        <select value={gender} onChange={onChangeGender}>
+          <option value=""> 밝히지 않음</option>
+          <option value="female"> 여성</option>
+          <option value="male"> 남성</option>
+        </select>
+      </div>
+      <div>
+        <textarea value={bio} onChange={onChangeBio}/>
+      </div>
     </div>
   );
 }
+
 
